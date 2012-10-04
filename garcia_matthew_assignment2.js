c84392cc85945e1758060d165b927e71879256c4
purifiedgenius <purifiedgenius@fullsail.edu>
2012-10-04 16:11:03 -0700

    Added my comments and variables
    The comments include all the project details.

--- garcia_matthew_assignment2.js
+++ garcia_matthew_assignment2.js
@@ -1,1 +1,13 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Matthew Garcia
+// SDI 1210
+// Project 2
+
+// Declare variables
+
+var apostleName = "Paul";
+var ministryDays = 34;
+var epistles = ["Ephesians", "Philippians", "Galatians", "Philemon"];
+
+// Procedure
